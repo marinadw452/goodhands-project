5b4f399af453153092d8977137b1be61ddc4de6b
marinadw452 <maaara62626@gmail.com>
2025-10-15 02:14:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("الموقع يعمل بنجاح ✅");
+const slides = document.querySelectorAll('.slide');
+let current = 0;
+const intervalTime = 3000; // مدة عرض كل صورة بالمللي ثانية
+
+setInterval(() => {
+  slides[current].classList.remove('active');
+  current = (current + 1) % slides.length;
+  slides[current].classList.add('active');
+}, intervalTime);
