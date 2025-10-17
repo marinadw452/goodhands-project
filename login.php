2c9bfa0aec7539c4a2535462fd0d270ac57321a8
marinadw452 <maaara62626@gmail.com>
2025-10-17 17:33:33 +0300

    Update login.php

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>تسجيل الدخول - أيدي طيبة</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="login.css">
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 </head>
 <body>
