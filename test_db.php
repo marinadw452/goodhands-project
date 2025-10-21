d237954bc3993cefefcd773fd37e684419e17727
marinadw452 <maaara62626@gmail.com>
2025-10-21 13:11:27 +0300

    Delete test_db.php

--- test_db.php
+++
@@ -1,15 +0,0 @@
-<?php
-require_once "config.php";
-
-try {
-    $stmt = $conn->query("SELECT NOW()");
-    $time = $stmt->fetchColumn();
-    echo "✅ اتصال ناجح بقاعدة البيانات!<br>";
-    echo "🕒 الوقت الحالي في السيرفر: " . $time;
-} catch (PDOException $e) {
-    echo "<pre>";
-    echo "❌ فشل الاتصال بقاعدة البيانات:\n";
-    echo $e->getMessage();
-    echo "</pre>";
-}
-?>
