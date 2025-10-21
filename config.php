3ed1642670d5ffc0ebc62562aa4beb586242953d
marinadw452 <maaara62626@gmail.com>
2025-10-21 13:17:33 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,20 @@
 <?php
-$host = getenv("PGHOST") 
-$port = getenv("PGPORT") 
-$dbname = getenv("PGDATABASE") 
-$user = getenv("PGUSER") 
-$password = getenv("PGPASSWORD")
+$host = getenv("PGHOST");
+$port = getenv("PGPORT");
+$dbname = getenv("PGDATABASE");
+$user = getenv("PGUSER");
+$password = getenv("PGPASSWORD");
 
 try {
-    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
 } catch(PDOException $e) {
     die("فشل الاتصال بقاعدة البيانات: " . $e->getMessage());
 }
