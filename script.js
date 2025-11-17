5c054cdaccc4ae5f5d94c49271079616d44c8d2d
marinadw452 <maaara62626@gmail.com>
2025-11-17 22:20:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const slides = document.querySelectorAll('.slide');
 let current = 0;
 const intervalTime = 3000;
-
 setInterval(() => {
   slides[current].classList.remove('active');
   current = (current + 1) % slides.length;
@@ -10,10 +9,12 @@ setInterval(() => {
 }, intervalTime);
 
 // ===== Sidebar Login =====
-function openLoginPanel() {
-  document.getElementById("loginPanel").style.right = "0";
-}
-
-function closeLoginPanel() {
-  document.getElementById("loginPanel").style.right = "-350px";
-}
+document.getElementById('side-login-btn').onclick = function() {
+  document.getElementById('sidebar-login').style.display = 'flex';
+};
+document.getElementById('close-sidebar').onclick = function() {
+  document.getElementById('sidebar-login').style.display = 'none';
+};
+document.getElementById('sidebar-login').addEventListener('click', function(e) {
+  if(e.target === this) this.style.display = 'none';
+});
