68df9a78bc9a5cd8701fdacd3d08227223fc6fbf
marinadw452 <maaara62626@gmail.com>
2025-11-17 21:05:02 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
-    last_login TIMESTAMP,
-    is_active BOOLEAN DEFAULT TRUE
+    email VARCHAR(150),
+    password TEXT NOT NULL,
+    role VARCHAR(20) NOT NULL CHECK (role IN ('user', 'seller')) DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT NOW()
 );
