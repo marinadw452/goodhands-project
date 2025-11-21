1311664724537080bd72fae3184ed8db49e76ee8
marinadw452 <maaara62626@gmail.com>
2025-11-21 19:52:14 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,8 +1,9 @@
+-- create_users.sql
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     user_type VARCHAR(10) NOT NULL CHECK (user_type IN ('buyer', 'seller')),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
