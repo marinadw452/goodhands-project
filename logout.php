8c7e4890f3f03b2e2329ded3b2a1b38cfcde770b
marinadw452 <maaara62626@gmail.com>
2025-12-02 16:31:00 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,2 @@
-<?php
-session_start();
-session_destroy();
-header("Location: index.php");
-exit;
-?>
+<!-- logout.php -->
+<?php session_start(); session_destroy(); header("Location: index.php"); ?>
